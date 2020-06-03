7d559d8eb1e53711766dbac3d4c91c052acc7466
pengyunshan <pengyunshan94@163.com>
2020-06-03 15:28:46 +0800

    修改matrix为array 添加新的文件读取函数

--- simrank.py
+++ simrank.py
@@ -4,47 +4,63 @@ import numpy as np
 class SimRank:
 
     def __init__(self):
-        self.queries = None
-        self.ads = None
+        self.users = []
+        self.items = []
         self.graph = None
-        self.query_sim = None
-        self.ad_sim = None
+        self.user_sim = None
+        self.item_sim = None
 
-    def load_data(self, path='test/sample1.txt'):
+    def load_data(self, path='data/sample1.txt'):
         with open(path, 'r') as log_fp:
             logs_tuple = [tuple(log.strip().split(",")) for log in log_fp.readlines()]
-        self.queries = list(set([log[0] for log in logs_tuple ]))
-        self.ads = list(set([log[1] for log in logs_tuple ]))
+        self.users = list(set([log[0] for log in logs_tuple]))
+        self.items = list(set([log[1] for log in logs_tuple]))
         # Graph means the relations number
-        self.graph = np.matrix(np.zeros([len(self.queries), len(self.ads)]))
+        self.graph = np.array(np.zeros([len(self.users), len(self.items)]))
         for log in logs_tuple:
             query = log[0]
             ad = log[1]
-            q_i = self.queries.index(query)
-            a_j = self.ads.index(ad)
+            q_i = self.users.index(query)
+            a_j = self.items.index(ad)
             self.graph[q_i, a_j] += 1
         print(self.graph)
         
-        self.query_sim = np.matrix(np.identity(len(self.queries)))
-        self.ad_sim = np.matrix(np.identity(len(self.ads)))
+        self.user_sim = np.array(np.identity(len(self.users)))
+        self.item_sim = np.array(np.identity(len(self.items)))
+
+    def load_serials(self, path="../data/20200525"):
+        with open(path, "r", encoding="utf-8") as f:
+            logs_tuple = [(line.strip("\n").split()[0],line.strip("\n").split()[1:]) for line in f.readlines()]
+            self.users = list(set([log[0] for log in logs_tuple]))
+            click_serials = list([log[1] for log in logs_tuple])
+            self.items = list(set([item for serials in click_serials for item in serials]))
+            self.graph = np.array(np.zeros([len(self.users), len(self.items)]))
+            for log in logs_tuple:
+                user, items = log[0], log[1]
+                u_i = self.users.index(user)
+                for item in items:
+                    i_j = self.items.index(item)
+                    self.graph[u_i, i_j] += 1
+            self.user_sim = np.array(np.identity(len(self.users)))
+            self.item_sim = np.array(np.identity(len(self.items)))
 
     def get_ads_num(self, query):
-        q_i = self.queries.index(query)
+        q_i = self.users.index(query)
         return self.graph[q_i]
     
     def get_queries_num(self, ad):
-        a_j = self.ads.index(ad)
+        a_j = self.items.index(ad)
         return self.graph.transpose()[a_j]
     
     def get_ads(self, query):
-        series = self.get_ads_num(query).tolist()[0]
-        return [ self.ads[x] for x in range(len(series)) if series[x] > 0 ]
+        series = self.get_ads_num(query).tolist()
+        return [self.items[i] for i in range(len(series)) if series[i] > 0]
     
     def get_queries(self, ad):
-        series = self.get_queries_num(ad).tolist()[0]
-        return [self.queries[x] for x in range(len(series)) if series[x] > 0]
+        series = self.get_queries_num(ad).tolist()
+        return [self.users[i] for i in range(len(series)) if series[i] > 0]
 
-    def query_simrank(self, q1, q2, C):
+    def user_simrank(self, q1, q2, C):
         """
         in this, graph[q_i] -> connected ads
         """
@@ -54,17 +70,18 @@ class SimRank:
         print "q2.ads"
         print get_ads_num(q2).tolist()
         """
-        if q1 == q2 : return 1
+        if q1 == q2:
+            return 1
         prefix = C / (self.get_ads_num(q1).sum() * self.get_ads_num(q2).sum())
         postfix = 0
         for ad_i in self.get_ads(q1):
             for ad_j in self.get_ads(q2):
-                i = self.ads.index(ad_i)
-                j = self.ads.index(ad_j)
-                postfix += self.ad_sim[i, j]
+                i = self.items.index(ad_i)
+                j = self.items.index(ad_j)
+                postfix += self.item_sim[i, j]
         return prefix * postfix
 
-    def ad_simrank(self, a1, a2, C):
+    def item_simrank(self, a1, a2, C):
         """
         in this, graph need to be transposed to make ad to be the index
         """
@@ -79,38 +96,37 @@ class SimRank:
         postfix = 0
         for query_i in self.get_queries(a1):
             for query_j in self.get_queries(a2):
-                i = self.queries.index(query_i)
-                j = self.queries.index(query_j)
-                postfix += self.query_sim[i,j]
+                i = self.users.index(query_i)
+                j = self.users.index(query_j)
+                postfix += self.user_sim[i, j]
         return prefix * postfix
 
     def simrank(self, C=0.8, times=1):
         # global query_sim, ad_sim
         for run in range(times):
             # queries simrank
-            new_query_sim = np.matrix(np.identity(len(self.queries)))
-            for qi in self.queries:
-                for qj in self.queries:
-                    i = self.queries.index(qi)
-                    j = self.queries.index(qj)
-                    new_query_sim[i,j] = self.query_simrank(qi, qj, C)
+            new_user_sim = np.array(np.identity(len(self.users)))
+            for qi in self.users:
+                for qj in self.users:
+                    i = self.users.index(qi)
+                    j = self.users.index(qj)
+                    new_user_sim[i, j] = self.user_simrank(qi, qj, C)
             # ads simrank
-            new_ad_sim = np.matrix(np.identity(len(self.ads)))
-            for ai in self.ads:
-                for aj in self.ads:
-                    i = self.ads.index(ai)
-                    j = self.ads.index(aj)
-                    new_ad_sim[i,j] = self.ad_simrank(ai, aj, C)
-            self.query_sim = new_query_sim
-            self.ad_sim = new_ad_sim
+            new_item_sim = np.array(np.identity(len(self.items)))
+            for ai in self.items:
+                for aj in self.items:
+                    i = self.items.index(ai)
+                    j = self.items.index(aj)
+                    new_item_sim[i, j] = self.item_simrank(ai, aj, C)
+            self.user_sim = new_user_sim
+            self.item_sim = new_item_sim
 
 
 if __name__ == '__main__':
     simrank = SimRank()
     simrank.load_data()
+    print(simrank.users)
+    print(simrank.items)
     simrank.simrank()
-#    print(queries)
-#    print(ads)
-#    simrank()
-#    print(query_sim)
-#    print(ad_sim)
+    print(simrank.user_sim)
+    print(simrank.item_sim)
